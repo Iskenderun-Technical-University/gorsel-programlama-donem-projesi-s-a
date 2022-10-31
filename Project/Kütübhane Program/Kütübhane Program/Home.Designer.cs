0cb52cf8bbb03e46c88ebf88f5c50fed5ecc9c1f
AHMED ALASSAF <ahmetalassaf45@gmail.com>
2022-10-31 19:32:56 +0300

    Ilk Commit

---
+++ Project/Kütübhane Program/Kütübhane Program/Home.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace Kütübhane_Program
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
+            this.btnbaslat = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
+            this.SuspendLayout();
+            // 
+            // btnbaslat
+            // 
+            this.btnbaslat.AllowAnimations = true;
+            this.btnbaslat.AllowMouseEffects = true;
+            this.btnbaslat.AllowToggling = false;
+            this.btnbaslat.AnimationSpeed = 200;
+            this.btnbaslat.AutoGenerateColors = false;
+            this.btnbaslat.AutoRoundBorders = true;
+            this.btnbaslat.AutoSizeLeftIcon = true;
+            this.btnbaslat.AutoSizeRightIcon = true;
+            this.btnbaslat.BackColor = System.Drawing.Color.Transparent;
+            this.btnbaslat.BackColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnbaslat.BackgroundImage")));
+            this.btnbaslat.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnbaslat.ButtonText = "BAŞLAT";
+            this.btnbaslat.ButtonTextMarginLeft = 0;
+            this.btnbaslat.ColorContrastOnClick = 45;
+            this.btnbaslat.ColorContrastOnHover = 45;
+            this.btnbaslat.Cursor = System.Windows.Forms.Cursors.Hand;
+            borderEdges1.BottomLeft = true;
+            borderEdges1.BottomRight = true;
+            borderEdges1.TopLeft = true;
+            borderEdges1.TopRight = true;
+            this.btnbaslat.CustomizableEdges = borderEdges1;
+            this.btnbaslat.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnbaslat.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnbaslat.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnbaslat.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnbaslat.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+            this.btnbaslat.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbaslat.ForeColor = System.Drawing.Color.White;
+            this.btnbaslat.IconLeftAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnbaslat.IconLeftCursor = System.Windows.Forms.Cursors.Default;
+            this.btnbaslat.IconLeftPadding = new System.Windows.Forms.Padding(11, 3, 3, 3);
+            this.btnbaslat.IconMarginLeft = 11;
+            this.btnbaslat.IconPadding = 10;
+            this.btnbaslat.IconRightAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnbaslat.IconRightCursor = System.Windows.Forms.Cursors.Default;
+            this.btnbaslat.IconRightPadding = new System.Windows.Forms.Padding(3, 3, 7, 3);
+            this.btnbaslat.IconSize = 25;
+            this.btnbaslat.IdleBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.IdleBorderRadius = 63;
+            this.btnbaslat.IdleBorderThickness = 1;
+            this.btnbaslat.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.IdleIconLeftImage = null;
+            this.btnbaslat.IdleIconRightImage = null;
+            this.btnbaslat.IndicateFocus = false;
+            this.btnbaslat.Location = new System.Drawing.Point(290, 533);
+            this.btnbaslat.Name = "btnbaslat";
+            this.btnbaslat.OnDisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.btnbaslat.OnDisabledState.BorderRadius = 1;
+            this.btnbaslat.OnDisabledState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnbaslat.OnDisabledState.BorderThickness = 1;
+            this.btnbaslat.OnDisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.btnbaslat.OnDisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.btnbaslat.OnDisabledState.IconLeftImage = null;
+            this.btnbaslat.OnDisabledState.IconRightImage = null;
+            this.btnbaslat.onHoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.onHoverState.BorderRadius = 1;
+            this.btnbaslat.onHoverState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnbaslat.onHoverState.BorderThickness = 1;
+            this.btnbaslat.onHoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.onHoverState.ForeColor = System.Drawing.Color.White;
+            this.btnbaslat.onHoverState.IconLeftImage = null;
+            this.btnbaslat.onHoverState.IconRightImage = null;
+            this.btnbaslat.OnIdleState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.OnIdleState.BorderRadius = 1;
+            this.btnbaslat.OnIdleState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnbaslat.OnIdleState.BorderThickness = 1;
+            this.btnbaslat.OnIdleState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.OnIdleState.ForeColor = System.Drawing.Color.White;
+            this.btnbaslat.OnIdleState.IconLeftImage = null;
+            this.btnbaslat.OnIdleState.IconRightImage = null;
+            this.btnbaslat.OnPressedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.OnPressedState.BorderRadius = 1;
+            this.btnbaslat.OnPressedState.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.btnbaslat.OnPressedState.BorderThickness = 1;
+            this.btnbaslat.OnPressedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnbaslat.OnPressedState.ForeColor = System.Drawing.Color.White;
+            this.btnbaslat.OnPressedState.IconLeftImage = null;
+            this.btnbaslat.OnPressedState.IconRightImage = null;
+            this.btnbaslat.Size = new System.Drawing.Size(227, 65);
+            this.btnbaslat.TabIndex = 0;
+            this.btnbaslat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnbaslat.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnbaslat.TextMarginLeft = 0;
+            this.btnbaslat.TextPadding = new System.Windows.Forms.Padding(0);
+            this.btnbaslat.UseDefaultRadiusAndThickness = true;
+            this.btnbaslat.Click += new System.EventHandler(this.bunifuButton1_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kütübhane_Program.Properties.Resources.KÜTÜPHANE_ILK;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1300, 700);
+            this.Controls.Add(this.btnbaslat);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.UI.WinForms.BunifuButton.BunifuButton btnbaslat;
+    }
+}
+
---
+++ Project/Kütübhane Program/Kütübhane Program/KiTAPLAR.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütübhane_Program
+{
+    public partial class KiTAPLAR : Form
+    {
+        public KiTAPLAR()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            konular fr3 = new konular();
+            fr3.Show();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
